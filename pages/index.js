c21055255704b72ac214149703448379103a3418
Gustavo Silva <guss.silva@hotmail.com>
2021-05-26 12:19:22 -0300

    index page set with button and texts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import Container from 'react-bootstrap/Container';
+import { Button, Row, Col } from 'react-bootstrap/';
+import Link from 'next/link';
+import { FaCrosshairs } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <>
-    </>
+    <Row className='mt-8 justify-content-center'>
+      <Col md={7} xs={12} className='text-center'>
+        <h1 className='fw-bolder text-custom-gray-darker mb-5 1h-base display-5'>
+          Comida saudável e gostosa direto na sua casa
+        </h1>
+        <Link href='/restaurants'>
+          <Button variant='custom-red' size='lg' className='text-white'>
+            <FaCrosshairs className='pb-1'></FaCrosshairs>
+            <span className='px-2'>ENCONTRE AGORA</span>
+          </Button>
+        </Link>
+      </Col>
+    </Row>
   )
 }
